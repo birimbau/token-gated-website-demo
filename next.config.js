386bcf49c1ca4b26d842d05353ec6fff3aba6932
Tiago Neto <tiagompbneto@gmail.com>
2023-07-04 18:57:02 +0100

    updated package version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['collabland-tokengate-react-context'],
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
